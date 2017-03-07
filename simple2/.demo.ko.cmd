8f258bcf4b34a43df9f58409c9f558b4a4e83ad9
Anan <shaonan0714@thundersoft.com>
2017-03-07 14:03:54 +0800

    add application to learn mmap

---
+++ simple2/.demo.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/sn/shinq/ldd3-examples-3.x/simple2/demo.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/sn/shinq/ldd3-examples-3.x/simple2/demo.ko /home/sn/shinq/ldd3-examples-3.x/simple2/demo.o /home/sn/shinq/ldd3-examples-3.x/simple2/demo.mod.o
---
+++ simple2/.tmp_versions/demo.mod
@@ -0,0 +1,2 @@
+/home/sn/shinq/ldd3-examples-3.x/simple2/demo.ko
+/home/sn/shinq/ldd3-examples-3.x/simple2/demo.o
